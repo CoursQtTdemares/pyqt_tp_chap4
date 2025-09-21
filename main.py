91d8da7188859b895319be15b3f529237447a610
tdemares <timothee.demares@gmail.com>
2025-09-21 21:28:48 +0200

    Chargement dynamique en Python

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-def main() -> None:
+import sys
+from pathlib import Path
+
+from PyQt6 import uic
+from PyQt6.QtWidgets import QApplication
+
+WORKSPACE_DIR = Path(__file__).parent
+UI_PATH = WORKSPACE_DIR / "src" / "simple_interface.ui"
+
+
+def main() -> int:
     """Entry point for pyqt_tp_chap4."""
-    ...
+    app = QApplication(sys.argv)
+
+    window = uic.loadUi(UI_PATH)
+    window.show()
+
+    return app.exec()
 
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
